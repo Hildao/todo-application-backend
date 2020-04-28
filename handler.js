24977ec06d7890de1a8cc0ee55fa69479f9fa1a2
Hilda Osazuwa <hildaosazuwa2@yahoo.com>
2020-04-28 18:53:48 +0100

    Updated endpoints

--- handler.js
+++ handler.js
@@ -6,6 +6,14 @@ const serverlessHttp = require('serverless-http');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'Tasks',
+});
 
 // Logically separate 4 sections of code according to the method of the HTTP request received
 
@@ -17,50 +25,86 @@ app.use(bodyParser.json());
 
 app.get('/tasks', function (request, response) {
   // Should make a SELECT * FROM Tasks query to the DB and return the results
-  response.status(200).send({
-    tasks: [
-      {
-        Id: 1,
-        Description: 'Wash the dishes',
-        DueDate: '2020-04-12',
-        Completed: true,
-        UserId: 1
-      },
-      {
-        Id: 2,
-        Description: 'Learn React',
-        DueDate: '2020-04-12',
-        Completed: true,
-        UserId: 1
-      },
-      {
-        Id: 3,
-        Description: 'call the Dentist',
-        DueDate: '2020-04-13',
-        Completed: false,
-        UserId: 2
-      },
-    ],
+  connection.query('SELECT * FROM Tasks', function (err, data) {
+    if (err) {
+      console.log('Error from MySQL', err);
+      response.status(500).send(err);
+    } else {
+      response.status(200).send(data);
+    }
   });
 });
 
 app.delete('/tasks/:id', function (request, response) {
   const id = request.params.id;
-  //  Should delete the task with the specified ID from the database
-  response.status(200).send(`Deleted task with ID ${id}!`);
+
+  // Should delete the task with the specified ID from the database
+  // Write a query in SQL
+  // Escape the id that is provided by the user
+  // Send back 200 status if successful
+  const query = `DELETE FROM Tasks WHERE TaskId = ?`;
+  connection.query(
+    query, [id],
+    function (err) {
+      if (err) {
+        console.log('Error from MySQL', err);
+        response.status(500).send(err);
+      } else {
+        response.status(200).send(`Deleted Task with ID: ${id}`);
+      }
+    });
 });
 
 app.post('/tasks', function (request, response) {
-  const data = request.body;  //{}
-  //  Should INSERT INTO the database the new task
-  response.status(201).send(`New task of ${data.text} created`);
+  const data = request.body;
+
+  //  SQL Injection - avoid this by "escaping" user-provided values
+  const query = `INSERT INTO Tasks (Description, DueDate, Completed) VALUES (?, ?, ?)`;
+  connection.query(
+    query,
+    [data.Description, data.DueDate, false],
+    function (err, results) {
+      if (err) {
+        console.log('Error from MySQL', err);
+        response.status(500).send(err);
+      } else {
+        //  Send back the newly created task
+        //  Because the frontend (or whatever client) might want to know the ID
+        connection.query(
+          `SELECT * FROM Tasks WHERE TaskId = ${results.insertId}`,
+          function (err, results) {
+            if (err) {
+              console.log('Error from MySQL', err);
+              response.status(500).send(err);
+            } else {
+              response.status(201).send(results[0]);
+            }
+          }
+        );
+      }
+    });
 });
 
 app.put('/tasks/:id', function (request, response) {
   const id = request.params.id;
   const data = request.body;
-  //  Should UPDATE a task in the DB
-  response.status(200).send(`Updated task with ID ${id} and data ${JSON.stringify(data)}`);
+  // Write an SQL query to update the fields provided in the request for the task WHERE TaskId = id
+  // Remember to escape user-provided values
+  // Send back 200 (not the updated task)
+
+  const query = 'UPDATE Tasks SET Description = ?, DueDate = ?, Completed = ?, UserId = ? WHERE TaskId = ?';
+  connection.query(
+    query, [data.Description, data.DueDate, data.Completed, data.UserId, id],
+    function (err) {
+      if (err) {
+        console.log('Error from MySQL', err);
+        response.status(500).send(err);
+      } else {
+        response
+          .status(200)
+          .send(`Updated task with ID ${id} and data ${JSON.stringify(data)}`);
+      }
+    });
 });
 
 module.exports.app = serverlessHttp(app);
